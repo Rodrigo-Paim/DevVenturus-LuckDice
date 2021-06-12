e1e6819e0c4ade7f0ab0fa538ba858b7065db9b4
Rodrigo-Paim <rodrigo_vasconceloss@hotmail.com>
2021-06-12 16:09:13 -0300

    up

---
+++ AndroidStudioProjects/LuckDices/app/src/main/java/com/devventure/luckdices/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.devventure.luckdices
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val playerName = intent.getStringExtra("playername")
+
+        val dice1 = findViewById<ImageView>(R.id.dice1_IV)
+        val dice2 = findViewById<ImageView>(R.id.dice2_IV)
+        val btn = findViewById<Button>(R.id.throw_btn)
+        val welcomeMessage = findViewById<TextView>(R.id.welcomeMessage)
+
+
+        val listImages = listOf(
+            R.drawable.dice_1,
+            R.drawable.dice_2,
+            R.drawable.dice_3,
+            R.drawable.dice_4,
+            R.drawable.dice_5,
+            R.drawable.dice_6,
+        )
+
+        btn.setOnClickListener{
+            dice1.setImageResource(listImages.random())
+            dice2.setImageResource(listImages.random())
+        }
+
+    }
+
+}
